dfad3a0617266681583df534a7065906eef25172
EC2 Default User <ec2-user@ip-172-31-38-211.us-east-2.compute.internal>
2021-02-21 22:02:37 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,18 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+       
+    # part1: 
+     @all_ratings = ['G','PG','PG-13','R']
+    if params[:ratings].nil?
+      @movies = Movie.order params[:order]
+    else
+      # filter movies based on ratings
+      array_ratings = params[:ratings].keys
+      @chosen_ratings = array_ratings
+      @movies = Movie.where(rating: array_ratings).order params[:order]
+    end
+
   end
 
   def new
