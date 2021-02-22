edb86be26d9f762c2fa88ad91e631dc8480d6bf8
EC2 Default User <ec2-user@ip-172-31-38-211.us-east-2.compute.internal>
2021-02-22 01:03:26 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,8 +7,53 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings=Movie.all_ratings
-    @movies = Movie.order(params[:order])
+    array_ratings = params[:ratings]
+    @all_ratings = Movie.all_ratings
+    
+    if (request.referrer).nil?
+      session.clear
+    end
+
+
+    # Remember the sorting and filtering settings
+    
+    session[:ratings] = params[:ratings] unless params[:ratings].nil?
+    session[:sort] = params[:sort] unless params[:sort].nil?
+    sort_by = params[:sort]
+
+    # apply settings from session when the incoming URI doesn’t have params
+    if ((params[:ratings].nil? && !session[:ratings].nil?) || (params[:order].nil? && !session[:order].nil?))
+      @ratings_to_show = Movie.all_ratings
+      @movies = Movie.with_ratings(@ratings_to_show, session[:sort])
+      session[:ratings] = params[:rating]
+
+
+    #when URI has params, the new settings should be remembered in the session.
+    elsif (params[:ratings].nil? && !session[:ratings].nil?) || (params[:sort].nil? && !session[:sort].nil?)
+      redirect_to movies_path("ratings" => session[:ratings], "sort" => session[:sort])
+    
+    else
+      if !params[:ratings].nil?
+        array_ratings  = params[:ratings].keys
+      else
+        array_ratings  = @all_ratings
+      end
+      if sort_by == 'title'
+        @sort_by = sort_by
+        @highlight = 'title'
+      elsif sort_by=='release_date'
+        @sort_by = sort_by
+        @highlight = 'release_date'
+      else
+        @sort_by = ""
+        @ratings_to_show = array_ratings  
+        @highlight = nil
+      end
+      
+      @ratings_to_show = array_ratings  
+      @movies = Movie.with_ratings(@ratings_to_show, @sort_by)
+    end
+  
   end
   
   
